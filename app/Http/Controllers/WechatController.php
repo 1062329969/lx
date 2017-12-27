cc35b252e84dd0b1fd24a1097e5d83f5f0ca8bfa
lixiao <13261597785@163.com>
2017-12-27 12:49:19 +0800

    是

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -14,7 +14,7 @@ class WechatController extends Controller
      */
     public function serve()
     {
-       // return 123;
+       return 123;die;
        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
        $wechat = app('wechat');
