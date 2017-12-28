883af2938bac82a747a40508665636b8e44dda0f
lixiao <13261597785@163.com>
2017-12-28 14:11:01 +0800

    1233333

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -25,15 +25,11 @@ class WeChatController extends Controller
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
-        $app->template_message->send([
-            'touser' => 'ox_re1Zh30FKCubPhNHeIzdOvj3A',
-            'template_id' => '3As8lqURZg1rMUSdX7wVan131X1OEepEOaPTEgNjVzo',
-            'url' => 'https://easywechat.org',
-            'data' => [
-                'key1' => 'VALUE',
-                'key2' => 'VALUE2',
-        ],
-    ]);
+        $app->server->push(function($message){
+            return "欢迎关注你，欢迎来到朕的世界！";
+        });
+
+
         return $app->server->serve();
     }
 
@@ -49,7 +45,7 @@ class WeChatController extends Controller
     {
         $app = app('wechat.official_account');
         $app->template_message->send([
-            'touser' => 'user-openid',
+            'touser' => 'wx083fd9c68238faaf',
             'template_id' => '3As8lqURZg1rMUSdX7wVan131X1OEepEOaPTEgNjVzo',
             'url' => 'https://easywechat.org',
             'data' => [
