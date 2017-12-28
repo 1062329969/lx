70f63fda02ab6e42962b88a2ac081cd0c5ff534e
lixiao <13261597785@163.com>
2017-12-28 14:53:29 +0800

    湿哒哒的

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -44,9 +44,34 @@ class WeChatController extends Controller
     public function news()
     {
         $app = app('wechat.official_account');
-        $user = $app->user->get(1);
-        dd($res);die;
-        return $app->server->serve();
+        $buttons = [
+            [
+                "type" => "click",
+                "name" => "今日歌曲",
+                "key"  => "V1001_TODAY_MUSIC"
+            ],
+            [
+                "name"       => "菜单",
+                "sub_button" => [
+                    [
+                        "type" => "view",
+                        "name" => "搜索",
+                        "url"  => "http://www.soso.com/"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "视频",
+                        "url"  => "http://v.qq.com/"
+                    ],
+                    [
+                        "type" => "click",
+                        "name" => "赞一下我们",
+                        "key" => "V1001_GOOD"
+                    ],
+                ],
+            ],
+        ];
+        $app->menu->create($buttons);
         }
 
 }
