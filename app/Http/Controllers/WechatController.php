fe76ffae9be7099f4f7225f8cca611b4b4db074f
lixiao <13261597785@163.com>
2017-12-28 15:02:09 +0800

    的

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -25,34 +25,9 @@ class WeChatController extends Controller
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
-        $buttons = [
-            [
-                "type" => "click",
-                "name" => "今日歌曲",
-                "key"  => "V1001_TODAY_MUSIC"
-            ],
-            [
-                "name"       => "菜单",
-                "sub_button" => [
-                    [
-                        "type" => "view",
-                        "name" => "搜索",
-                        "url"  => "http://www.soso.com/"
-                    ],
-                    [
-                        "type" => "view",
-                        "name" => "视频",
-                        "url"  => "http://v.qq.com/"
-                    ],
-                    [
-                        "type" => "click",
-                        "name" => "赞一下我们",
-                        "key" => "V1001_GOOD"
-                    ],
-                ],
-            ],
-        ];
-        $app->menu->create($buttons);
+        $app->server->push(function($message){
+            return "欢迎关注你，欢迎来到朕的世界！";
+        });
 
 
         return $app->server->serve();
