e1851e635c6ac15cec4e233c837289ae89024c87
lixiao <13261597785@163.com>
2017-12-29 13:52:09 +0800

    啊大大

--- app/Http/Controllers/FromController.php
+++ app/Http/Controllers/FromController.php
@@ -7,7 +7,35 @@ use Log;
         public function index()
         {
             $app = app('wechat.official_account');
-
+            $buttons = [
+                [
+                    "type" => "click",
+                    "name" => "今日歌曲",
+                    "key" => "V1001_TODAY_MUSIC"
+                ],
+                [
+                    "name" => "菜单",
+                    "sub_button" => [
+                        [
+                            "type" => "view",
+                            "name" => "搜索",
+                            "url" => "http://www.soso.com/"
+                        ],
+                        [
+                            "type" => "view",
+                            "name" => "视频",
+                            "url" => "http://v.qq.com/"
+                        ],
+                        [
+                            "type" => "click",
+                            "name" => "赞一下我们",
+                            "key" => "V1001_GOOD"
+                        ],
+                    ],
+                ],
+            ];
+            $app->menu->add($buttons);
+            //var_dump($res);
 
         }
     }
--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -22,35 +22,15 @@ class WeChatController extends Controller
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
-        $buttons = [
-            [
-                "type" => "click",
-                "name" => "今日歌曲",
-                "key" => "V1001_TODAY_MUSIC"
-            ],
-            [
-                "name" => "菜单",
-                "sub_button" => [
-                    [
-                        "type" => "view",
-                        "name" => "搜索",
-                        "url" => "http://www.soso.com/"
-                    ],
-                    [
-                        "type" => "view",
-                        "name" => "视频",
-                        "url" => "http://v.qq.com/"
-                    ],
-                    [
-                        "type" => "click",
-                        "name" => "赞一下我们",
-                        "key" => "V1001_GOOD"
-                    ],
-                ],
-            ],
-        ];
-        $res = $app->menu->create($buttons);
-        var_dump($res);
+        $app->server->push(function($message){
+            return "你好";
+        });
+
+        $app->server->push(function($message){
+            return "你发送了一张图片";
+        },Message::IMAGE);
+        return  $app->server->serve();
+      //$arr ->send();
     }
 
 
