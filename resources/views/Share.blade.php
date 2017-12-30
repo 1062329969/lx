710e2078409611e0f2822dc7557d918b15d25765
lixiao <13261597785@163.com>
2017-12-30 10:57:42 +0800

    啊大大

---
+++ resources/views/Share.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>JSSDK的使用</title>
+    <script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
+</head>
+<body>
+<script type="text/javascript" charset="utf-8">
+    wx.config({!! $config !!});
+</script>
+</body>
+</html>
